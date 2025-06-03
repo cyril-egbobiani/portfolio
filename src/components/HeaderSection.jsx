e8ac134424d6549104a0b2c9e5ffefb8ef5cf061
cyril-egbobiani <116189048+cyril-egbobiani@users.noreply.github.com>
2025-06-03 16:00:14 +0100

    update

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -65,7 +65,7 @@ export default function HeaderSection() {
       initial="hidden"
       animate="visible"
       variants={containerVariants}
-      className="flex flex-col items-center justify-center min-h-[60vh] mt-6 sm:mt-18 lg:mt-30"
+      className="flex flex-col items-center justify-center min-h-[60vh] mt-9 sm:mt-18 lg:mt-25"
     >
       {/* Card */}
       <motion.div
